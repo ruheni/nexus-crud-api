ca325b2a84152608c5c35029fb500bad4a490ce0
ruheni <aleruheni@outlook.com>
2021-01-08 10:11:26 +0300

    minor updates to schema

--- src/schema.ts
+++ src/schema.ts
@@ -1,9 +1,9 @@
 import { makeSchema } from 'nexus'
 import { nexusPrisma } from 'nexus-plugin-prisma'
-import Mutation from './Mutation'
-import Post from './Post'
-import Query from './Query'
-import User from './User'
+import Mutation from './graphql/Mutation'
+import Post from './graphql/Post'
+import Query from './graphql/Query'
+import User from './graphql/User'
 
 export const schema = makeSchema({
   types: [Post, User, Query, Mutation],
